f3f4404ff6efc0ed05b0fa498eabc2ecdcba2cf9
Andy Gilbert <agilbert@pivotal.io>
2018-11-21 08:25:01 -0800

    WIP
    - ListServiceBindingsByQuery

--- service_bindings_test.go
+++ service_bindings_test.go
@@ -6,6 +6,7 @@ import (
 	"testing"
 
 	. "github.com/smartystreets/goconvey/convey"
+	"net/url"
 )
 
 func TestListServiceBindings(t *testing.T) {
@@ -65,7 +66,45 @@ func TestListServiceBindings(t *testing.T) {
 		So(serviceBindings[1].ServiceInstanceUrl, ShouldEqual, "/v2/service_instances/c3023201-44f5-4dc8-a903-c69c9eba9809")
 
 	})
+
+	Convey("List a single page of service bindings", t, func() {
+		setup(MockRoute{
+			Method:      "GET",
+			Endpoint:    "/v2/service_bindings",
+			Output:      listServiceBindingsPayloadPage1,
+			Status:      200,
+			QueryString: "page=1",
+		}, t)
+		defer teardown()
+		c := &Config{
+			ApiAddress: server.URL,
+			Token:      "foobar",
+		}
+		client, err := NewClient(c)
+		So(err, ShouldBeNil)
+
+		serviceBindings, err := client.ListServiceBindingsByQuery(url.Values{
+			"page": []string{"1"},
+		})
+		So(err, ShouldBeNil)
+
+		So(len(serviceBindings), ShouldEqual, 1)
+		So(serviceBindings[0].Guid, ShouldEqual, "aa599bb3-4811-405a-bbe3-a68c7c55afc8")
+		So(serviceBindings[0].AppGuid, ShouldEqual, "b26e7e98-f002-41a8-a663-1b60f808a92a")
+		So(serviceBindings[0].ServiceInstanceGuid, ShouldEqual, "bde206e0-1ee8-48ad-b794-44c857633d50")
+		So(reflect.DeepEqual(
+			serviceBindings[0].Credentials,
+			map[string]interface{}{"creds-key-66": "creds-val-66"}), ShouldBeTrue)
+		So(serviceBindings[0].BindingOptions, ShouldBeEmpty)
+		So(serviceBindings[0].GatewayData, ShouldBeNil)
+		So(serviceBindings[0].GatewayName, ShouldEqual, "")
+		So(serviceBindings[0].SyslogDrainUrl, ShouldEqual, "")
+		So(serviceBindings[0].VolumeMounts, ShouldBeEmpty)
+		So(serviceBindings[0].AppUrl, ShouldEqual, "/v2/apps/b26e7e98-f002-41a8-a663-1b60f808a92a")
+		So(serviceBindings[0].ServiceInstanceUrl, ShouldEqual, "/v2/service_instances/bde206e0-1ee8-48ad-b794-44c857633d50")
+	})
 }
+
 func TestServiceBindingByGuid(t *testing.T) {
 	Convey("Service Binding By Guid", t, func() {
 		setup(MockRoute{"GET", "/v2/service_bindings/foo-bar-baz", serviceBindingByGuidPayload, "", 200, "", nil}, t)
